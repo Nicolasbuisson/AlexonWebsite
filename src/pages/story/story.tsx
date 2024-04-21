5e0cc2f1f6d5da0c8f0de9bda9665428e04fb951
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-04-21 12:12:45 -0400

    timelineItemExplicitGrid

--- src/pages/story/story.tsx
+++ src/pages/story/story.tsx
@@ -1,11 +1,12 @@
 import { Footer } from "../../components/footer/footer";
 import { Navigation } from "../../components/navigation/navigation";
 import { TimelineItem } from "../../components/timelineItem/timelineItem";
+import { TimelineItemExplicitGrid } from "../../components/timelineItemExplicitGrid/timelineItemExplicitGrid";
 import "./story.css";
 
 export const Story = () => {
   return (
-    <div>
+    <div className="story-container">
       <Navigation></Navigation>
       <section className="story-section-container">
         <img
@@ -21,7 +22,7 @@ export const Story = () => {
         </p>
       </section>
       <section className="story-section-container story-timeline-section">
-        <TimelineItem
+        {/* <TimelineItem
           title="June 2019"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Leo a diam
@@ -49,7 +50,36 @@ export const Story = () => {
             ></img>
           }
           isLast
-        ></TimelineItem>
+        ></TimelineItem> */}
+        <TimelineItemExplicitGrid
+          title="June 2019"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Leo a diam
+        sollicitudin tempor id. Morbi tempus iaculis urna id volutpat lacus. Est
+        lorem ipsum dolor sit amet consectetur adipiscing elit."
+          images={
+            <img
+              src={require("../../assets/images/lake.jpg")}
+              alt=""
+              className="story-timeline-image"
+            ></img>
+          }
+        ></TimelineItemExplicitGrid>
+        <TimelineItemExplicitGrid
+          title="December 2020"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Leo a diam
+        sollicitudin tempor id. Morbi tempus iaculis urna id volutpat lacus. Est
+        lorem ipsum dolor sit amet consectetur adipiscing elit."
+          images={
+            <img
+              src={require("../../assets/images/lake.jpg")}
+              alt=""
+              className="story-timeline-image"
+            ></img>
+          }
+          isLast
+        ></TimelineItemExplicitGrid>
       </section>
       <Footer></Footer>
     </div>
