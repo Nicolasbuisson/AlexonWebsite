affbaa55ce51692468941dcec998c57055a300cd
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-04-30 20:25:49 -0400

    story timeline 3 layout variations

--- src/pages/story/story.tsx
+++ src/pages/story/story.tsx
@@ -22,7 +22,7 @@ export const Story = () => {
         </p>
       </section>
       <section className="story-section-container story-timeline-section">
-        {/* <TimelineItem
+        <TimelineItem
           title="June 2019"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Leo a diam
@@ -32,7 +32,7 @@ export const Story = () => {
             <img
               src={require("../../assets/images/lake.jpg")}
               alt=""
-              className="story-timeline-image"
+              className="story-timeline-image rotate-2-deg-left"
             ></img>
           }
         ></TimelineItem>
@@ -43,15 +43,48 @@ export const Story = () => {
         sollicitudin tempor id. Morbi tempus iaculis urna id volutpat lacus. Est
         lorem ipsum dolor sit amet consectetur adipiscing elit."
           images={
-            <img
-              src={require("../../assets/images/lake.jpg")}
-              alt=""
-              className="story-timeline-image"
-            ></img>
+            <div className="story-timeline-two-image-container">
+              <img
+                src={require("../../assets/images/lake.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-two-image-first-image"
+              ></img>
+              <img
+                src={require("../../assets/images/rice-hills.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-two-image-second-image"
+              ></img>
+            </div>
+          }
+        ></TimelineItem>
+        <TimelineItem
+          title="January 2021"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Leo a diam
+        sollicitudin tempor id. Morbi tempus iaculis urna id volutpat lacus. Est
+        lorem ipsum dolor sit amet consectetur adipiscing elit."
+          images={
+            <div className="story-timeline-three-image-container">
+              <img
+                src={require("../../assets/images/lake.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-three-image-first-image z-index-2"
+              ></img>
+              <img
+                src={require("../../assets/images/road.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-three-image-second-image z-index-1"
+              ></img>
+              <img
+                src={require("../../assets/images/rice-hills.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-three-image-third-image z-index-2"
+              ></img>
+            </div>
           }
           isLast
-        ></TimelineItem> */}
-        <TimelineItemExplicitGrid
+        ></TimelineItem>
+        {/* <TimelineItemExplicitGrid
           title="June 2019"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Leo a diam
@@ -61,7 +94,7 @@ export const Story = () => {
             <img
               src={require("../../assets/images/lake.jpg")}
               alt=""
-              className="story-timeline-image"
+              className="story-timeline-image rotate-2-deg-left"
             ></img>
           }
         ></TimelineItemExplicitGrid>
@@ -72,14 +105,47 @@ export const Story = () => {
         sollicitudin tempor id. Morbi tempus iaculis urna id volutpat lacus. Est
         lorem ipsum dolor sit amet consectetur adipiscing elit."
           images={
-            <img
-              src={require("../../assets/images/lake.jpg")}
-              alt=""
-              className="story-timeline-image"
-            ></img>
+            <div className="story-timeline-two-image-container">
+              <img
+                src={require("../../assets/images/lake.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-two-image-first-image"
+              ></img>
+              <img
+                src={require("../../assets/images/rice-hills.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-two-image-second-image"
+              ></img>
+            </div>
+          }
+        ></TimelineItemExplicitGrid>
+        <TimelineItemExplicitGrid
+          title="January 2021"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Leo a diam
+        sollicitudin tempor id. Morbi tempus iaculis urna id volutpat lacus. Est
+        lorem ipsum dolor sit amet consectetur adipiscing elit."
+          images={
+            <div className="story-timeline-three-image-container">
+              <img
+                src={require("../../assets/images/lake.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-three-image-first-image z-index-2"
+              ></img>
+              <img
+                src={require("../../assets/images/road.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-three-image-second-image z-index-1"
+              ></img>
+              <img
+                src={require("../../assets/images/rice-hills.jpg")}
+                alt=""
+                className="story-timeline-image story-timeline-three-image-third-image z-index-2"
+              ></img>
+            </div>
           }
           isLast
-        ></TimelineItemExplicitGrid>
+        ></TimelineItemExplicitGrid> */}
       </section>
       <Footer></Footer>
     </div>
