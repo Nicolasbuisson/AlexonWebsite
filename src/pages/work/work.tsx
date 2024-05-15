bdc3eb71717dad0bdd6455373995a4a3dd25ae34
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-05-15 18:33:03 -0400

    work grid styling

--- src/pages/work/work.tsx
+++ src/pages/work/work.tsx
@@ -1,3 +1,31 @@
+import { Footer } from "../../components/footer/footer";
+import { Navigation } from "../../components/navigation/navigation";
+import "./work.css";
+
+// will have to dynamically create routes!
+// read from a json file?
+// picture, project, text, more than one picture?
 export const Work = () => {
-  return <div>work</div>;
+  const cards = Array(12).fill(0);
+  return (
+    <div className="work-container">
+      <Navigation></Navigation>
+      <section className="work-section-container">
+        <div className="projects-grid">
+          {cards.map((x, i) => {
+            return (
+              <div className="card stacked">
+                <img
+                  src={require("../../assets/images/sunset-purple-tree.jpg")}
+                  alt="project card"
+                ></img>
+                <h4>{`Project ${i}`}</h4>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <Footer></Footer>
+    </div>
+  );
 };
