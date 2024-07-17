245e44db6b25d10a83fccbe9c586d53a1ef0e426
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-07-17 14:32:16 -0400

    dynamic workItem

--- src/pages/workItem/workItem.tsx
+++ src/pages/workItem/workItem.tsx
@@ -1,58 +1,46 @@
 import { Footer } from "../../components/footer/footer";
 import { Navigation } from "../../components/navigation/navigation";
+import { WorkItemProps } from "../../types/work";
 import "./workItem.css";
 
-interface WorkItemProps {
-  title: string;
-  client: string;
-  description: string;
-  videoUrl: string;
-  stills: string[];
-}
-
 export const WorkItem = (props: WorkItemProps) => {
-  const { title, client, description, videoUrl, stills } = props;
+  const { title, client, description, videoUrl, gridImage, stills } = props;
 
   return (
     <div className="workItem-container">
       <Navigation transparent></Navigation>
       <section className="workItem-section">
-        <div className="workItem-title-background">
-          <h2>Title</h2>
+        <div
+          className="workItem-title-background"
+          style={{ backgroundImage: `url(/assets/images/${gridImage})` }}
+        >
+          <h2>{title}</h2>
         </div>
 
         <div className="video-container">
           <iframe
-            src="https://player.vimeo.com/video/694255333"
+            src={videoUrl}
             allow="autoplay; fullscreen"
             allowFullScreen
             className="video"
           ></iframe>
           <div className="video-details">
             <h4>
-              Client: <h4>Corona</h4>
+              Client: <h4>{client}</h4>
             </h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo a
-              diam sollicitudin tempor id. Morbi tempus iaculis urna id volutpat
-              lacus. Est lorem ipsum dolor sit amet consectetur adipiscing elit.
-            </p>
+            <p>{description}</p>
           </div>
         </div>
         <div className="stills-grid">
-          <img
-            src={require("../../assets/images/sunset-purple-tree.jpg")}
-            alt="stills grid picture"
-          ></img>
-          <img
-            src={require("../../assets/images/lake.jpg")}
-            alt="stills grid picture"
-          ></img>
-          <img
-            src={require("../../assets/images/road.jpg")}
-            alt="stills grid picture"
-          ></img>
+          {stills.map((stillImg) => {
+            return (
+              <img
+                key={"stills-" + stillImg.imageUrl}
+                src={`/assets/images/${stillImg.imageUrl}`}
+                alt="project stills picture"
+              ></img>
+            );
+          })}
         </div>
       </section>
       <Footer></Footer>
