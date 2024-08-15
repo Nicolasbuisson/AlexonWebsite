2769e9c0ee0707ef2551b2eea29b583f857d8019
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-08-15 16:04:20 -0400

    home page white/black alternating

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -18,14 +18,14 @@ export const Home = (props: HomeProps) => {
       <Navigation sticky title="Alexon" showIcons></Navigation>
       <section className="home-hero-section">
         <div className="home-hero-bg">
-          <div className="home-hero-text-container">
+          {/* <div className="home-hero-text-container">
             <div className="animated-text-container">
               <h3 className="animated-text">Artist</h3>
               <h3 className="animated-text">Videographer</h3>
               <h3 className="animated-text">Photographer</h3>
               <h3 className="animated-text">Athlete</h3>
             </div>
-          </div>
+          </div> */}
         </div>
       </section>
       <section className="home-services-section">
@@ -47,7 +47,7 @@ export const Home = (props: HomeProps) => {
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                stroke="#FFFFFF"
+                stroke="#000000"
               >
                 <g id="SVGRepo_bgCarrier" strokeWidth="0" />
                 <g
@@ -58,12 +58,12 @@ export const Home = (props: HomeProps) => {
                 <g id="SVGRepo_iconCarrier">
                   <path
                     d="M12 6.90909C10.8999 5.50893 9.20406 4.10877 5.00119 4.00602C4.72513 3.99928 4.5 4.22351 4.5 4.49965C4.5 6.54813 4.5 14.3034 4.5 16.597C4.5 16.8731 4.72515 17.09 5.00114 17.099C9.20405 17.2364 10.8999 19.0998 12 20.5M12 6.90909C13.1001 5.50893 14.7959 4.10877 18.9988 4.00602C19.2749 3.99928 19.5 4.21847 19.5 4.49461C19.5 6.78447 19.5 14.3064 19.5 16.5963C19.5 16.8724 19.2749 17.09 18.9989 17.099C14.796 17.2364 13.1001 19.0998 12 20.5M12 6.90909L12 20.5"
-                    stroke="#ffffff"
+                    stroke="#000000"
                     strokeLinejoin="round"
                   />
                   <path
                     d="M19.2353 6H21.5C21.7761 6 22 6.22386 22 6.5V19.539C22 19.9436 21.5233 20.2124 21.1535 20.0481C20.3584 19.6948 19.0315 19.2632 17.2941 19.2632C14.3529 19.2632 12 21 12 21C12 21 9.64706 19.2632 6.70588 19.2632C4.96845 19.2632 3.64156 19.6948 2.84647 20.0481C2.47668 20.2124 2 19.9436 2 19.539V6.5C2 6.22386 2.22386 6 2.5 6H4.76471"
-                    stroke="#ffffff"
+                    stroke="#000000"
                     strokeLinejoin="round"
                   />
                 </g>
@@ -84,7 +84,7 @@ export const Home = (props: HomeProps) => {
               >
                 <path
                   d="M5 18H15C16.1046 18 17 17.1046 17 16V8.57143V8C17 6.89543 16.1046 6 15 6H5C3.89543 6 3 6.89543 3 8V16C3 17.1046 3.89543 18 5 18Z"
-                  stroke="#FFFFFF"
+                  stroke="#000000"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
@@ -92,12 +92,12 @@ export const Home = (props: HomeProps) => {
                   cx="6.5"
                   cy="9.5"
                   r="0.5"
-                  stroke="#FFFFFF"
+                  stroke="#000000"
                   strokeLinejoin="round"
                 />
                 <path
                   d="M17 10L21 7V17L17 14"
-                  stroke="#FFFFFF"
+                  stroke="#000000"
                   strokeLinejoin="round"
                 />
               </svg>
@@ -109,7 +109,7 @@ export const Home = (props: HomeProps) => {
           and are entrepreneurs ourselves. We'll save you precious time and ensure that your content is effectively distributed"
             icon={
               <svg
-                fill="#ffffff"
+                fill="#000000"
                 version="1.1"
                 id="Layer_1"
                 xmlns="http://www.w3.org/2000/svg"
@@ -118,7 +118,7 @@ export const Home = (props: HomeProps) => {
                 xmlSpace="preserve"
                 width="90px"
                 height="90px"
-                stroke="#ffffff"
+                stroke="#000000"
               >
                 <g id="SVGRepo_bgCarrier" strokeWidth="0" />
                 <g
