584b63003e54f6772fd3bed842cfe6455d0fa685
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-08-19 20:17:26 -0400

    buttonBackgroundHoverEffect component

--- src/components/buttonBackgroundHoverEffect/buttonBackgroundHoverEffect.tsx
+++ src/components/buttonBackgroundHoverEffect/buttonBackgroundHoverEffect.tsx
@@ -6,5 +6,14 @@ interface IProps {
 
 export const ButtonBackgroundHoverEffect = (props: IProps) => {
   const { text } = props;
-  return <div className="button-background-effect">{text}</div>;
+  return (
+    <div className="button-background-effect-container">
+      <button className="button-background-effect">
+        <span className="button-background-effect-text">{text}</span>
+      </button>
+      <button className="black-background">
+        <span className="button-background-effect-text">{text}</span>
+      </button>
+    </div>
+  );
 };
