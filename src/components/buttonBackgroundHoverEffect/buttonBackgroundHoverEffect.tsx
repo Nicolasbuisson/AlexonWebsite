fd5922cf5ac38653eadbaf3fc51f71133bdc7747
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-09-02 15:22:28 -0400

    footer changes

--- src/components/buttonBackgroundHoverEffect/buttonBackgroundHoverEffect.tsx
+++ src/components/buttonBackgroundHoverEffect/buttonBackgroundHoverEffect.tsx
@@ -2,12 +2,13 @@ import "./buttonBackgroundHoverEffect.css";
 
 interface IProps {
   text: string;
+  className?: string;
 }
 
 export const ButtonBackgroundHoverEffect = (props: IProps) => {
-  const { text } = props;
+  const { text, className = "" } = props;
   return (
-    <div className="button-background-effect-container">
+    <div className={`button-background-effect-container ${className}`}>
       <button className="button-background-effect">{text}</button>
       <button className="black-background">{text}</button>
     </div>
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,16 +1,35 @@
+import { ButtonBackgroundHoverEffect } from "../buttonBackgroundHoverEffect/buttonBackgroundHoverEffect";
 import { Socials } from "../socials/socials";
 import "./footer.css";
 
 export const Footer = () => {
   return (
     <div className="footer-container">
-      <Socials></Socials>
-      <a href="https://www.instagram.com/alexoonnn/" className="footer-link">
-        @Alexon
-      </a>
-      <a href="mailto:alexon@gmail.com" className="footer-link">
-        alexon@gmail.com
-      </a>
+      <h1 className="footer-header">Let's work together</h1>
+      <ButtonBackgroundHoverEffect
+        text="Contact us"
+        className="footer-button"
+      ></ButtonBackgroundHoverEffect>
+      <div className="footer-items-container">
+        <div className="footer-newsletter-container">
+          <h4>Join our newsletter</h4>
+          <input placeholder="enter your email"></input>
+          <p>Stay up to date with our latest offers and projects!</p>
+        </div>
+        <div className="footer-socials-container">
+          <h4>Stay in touch</h4>
+          <a
+            href="https://www.instagram.com/alexoonnn/"
+            className="footer-link"
+          >
+            @Alexon
+          </a>
+          <a href="mailto:alexon@gmail.com" className="footer-link">
+            alexon@gmail.com
+          </a>
+          <Socials></Socials>
+        </div>
+      </div>
     </div>
   );
 };
