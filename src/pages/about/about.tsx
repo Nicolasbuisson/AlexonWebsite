b43cc4dbeb7b695249abc46538b147e3f22cc2b2
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-09-26 21:23:48 -0400

    draft about page

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -4,9 +4,20 @@ import "./about.css";
 
 export const About = () => {
   return (
-    <div className="about-container">
+    <div>
       <Navigation></Navigation>
-      <section className="about-section-container"></section>
+      <section className="about-section-container">
+        <div className="about-hero">
+          <h1 className="about-hero-header">About me</h1>
+          <div className="about-hero-picture"></div>
+          <p className="about-hero-paragraph">
+            A born artist hailing from the picturesque landscapes of the UK.
+            Through the lens, Gatsby weaves a tapestry of emotion, light, and
+            timeless moments, inviting you to embark on a visual journey where
+            each photograph tells a story of its own.
+          </p>
+        </div>
+      </section>
       <Footer></Footer>
     </div>
   );
