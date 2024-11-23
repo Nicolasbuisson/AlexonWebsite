906a691ffe1d6a64d24e6c76fd0190117be8d679
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-11-23 17:26:17 -0500

    work clients section

--- src/pages/work/work.tsx
+++ src/pages/work/work.tsx
@@ -1,9 +1,9 @@
 import { Footer } from "../../components/footer/footer";
 import { Navigation } from "../../components/navigation/navigation";
 import "./work.css";
-import { HorizontalScroller } from "../../components/horizontalScroller/horizontalScroller";
 import { WorkItemProps } from "../../types/work";
 import { Projects } from "../../components/projects/projects";
+import { LogoList } from "../../components/logoList/logoList";
 
 interface WorkProps {
   projects: WorkItemProps[];
@@ -19,40 +19,17 @@ export const Work = (props: WorkProps) => {
         <h2 className="work-title">Our Projects</h2>
         <Projects projects={projects}></Projects>
       </section>
-      <section className="work-section-container">
+      <section className="work-clients-section">
         <h2 className="work-clients-title">Our Clients</h2>
-        <HorizontalScroller>
-          <img
-            src={"/assets/clientLogos/Corona.png"}
-            alt="logo"
-            height="100px"
-          ></img>
-          <img
-            src={"/assets/clientLogos/Corona.png"}
-            alt="logo"
-            height="100px"
-          ></img>
-          <img
-            src={"/assets/clientLogos/Corona.png"}
-            alt="logo"
-            height="100px"
-          ></img>
-          <img
-            src={"/assets/clientLogos/Corona.png"}
-            alt="logo"
-            height="100px"
-          ></img>
-          <img
-            src={"/assets/clientLogos/Corona.png"}
-            alt="logo"
-            height="100px"
-          ></img>
-          <img
-            src={"/assets/clientLogos/Corona.png"}
-            alt="logo"
-            height="100px"
-          ></img>
-        </HorizontalScroller>
+        <LogoList
+          logos={[
+            "/assets/clientLogos/CoronaBlack.png",
+            "/assets/clientLogos/FourSeasonsBlack.png",
+            "/assets/clientLogos/ELLEBlack.png",
+            "/assets/clientLogos/JohnSummitBlack.png",
+            "/assets/clientLogos/McGillUniversityBlack.png",
+          ]}
+        ></LogoList>
       </section>
       <Footer></Footer>
     </div>
