c02006c305a0a77a07cfabd2df35003a5b6cc60c
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2025-03-02 15:43:09 -0500

    package info svg icon instead of asterik

--- src/components/package/package.tsx
+++ src/components/package/package.tsx
@@ -44,16 +44,51 @@ export const Package = (props: IPackage) => {
                 <p className="package-inclusion-list-item">
                   {item.item}
                   {item.hoverText && (
-                    <span className="package-inclusion-list-item-asterik">
-                      *
-                    </span>
+                    <>
+                      <svg
+                        className="package-inclusion-list-item-info-icon"
+                        width="22px"
+                        height="22px"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)"
+                      >
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                        <g
+                          id="SVGRepo_tracerCarrier"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        ></g>
+                        <g id="SVGRepo_iconCarrier">
+                          <circle
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="#ffffff"
+                            strokeWidth="1.5"
+                          ></circle>
+                          <path
+                            d="M12 17V11"
+                            stroke="#ffffff"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                          ></path>
+                          <circle
+                            cx="1"
+                            cy="1"
+                            r="1"
+                            transform="matrix(1 0 0 -1 11 9)"
+                            fill="#ffffff"
+                          ></circle>
+                        </g>
+                      </svg>
+                      <span className="package-inclusion-list-item-details">
+                        {item.hoverText}
+                      </span>
+                    </>
                   )}
                 </p>
-                {item.hoverText && (
-                  <p className="package-inclusion-list-item-details">
-                    {item.hoverText}
-                  </p>
-                )}
               </div>
             );
           })}
