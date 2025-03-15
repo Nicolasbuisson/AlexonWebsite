0df0e2d201d34a14b64ac5ad8191e47312bee86d
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2025-03-15 14:22:17 -0400

    cleanup contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,19 +8,12 @@ function Contact() {
     <div className="contact-container">
       <Navigation />
       <section className="contact-section">
-        <h2>contact a free consultation</h2>
+        <h2>book a free consultation</h2>
         <div className="calendly-container">
           <InlineWidget
             url="https://calendly.com/contact-alexonmedia/freeconsultation"
             styles={{ height: "100%", width: "100%" }}
           />
-        </div>
-        <div className="booking-alternatives">
-          <p>Or you can call us at:</p>
-          <a href="tel:+15145577790">Canada/US: +1 514 557 7790</a>
-          <a href="tel:+33783131299">France: +33 7 83 13 12 99</a>
-          <p>Or you can write to us at:</p>
-          <a href="mailto:contact@alexonmedia.com">contact@alexonmedia.com</a>
         </div>
       </section>
     </div>
