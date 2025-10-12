e901a594c415267d2927a20c2e983ecdeaf57ffa
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2025-10-12 09:34:47 -0400

    shorten aboutHeroText content

--- src/components/aboutHeroText/aboutHeroText.tsx
+++ src/components/aboutHeroText/aboutHeroText.tsx
@@ -15,7 +15,7 @@ export const AboutHeroText = (props: AboutHeroTextProps) => {
       <h1 className="about-hero-header">About Us</h1>
       <HeroParagraph
         text={
-          "Alexon Media crafts compelling visual narratives that captivate and convert. We are a team of storytellers, innovators, and digital strategists led by Alexon. Nimble, innovative, and always ahead of the curve, we turn ideas into impactful content that resonates across platforms. Let’s tell your story boldly, authentically, and with purpose."
+          "Alexon Media is a boutique video production agency creating luxury brand films and high-performance short-form content. We combine cinematic storytelling with strategic precision to move audiences and drive measurable brand growth."
         }
         className="about-hero-paragraph"
         scrollYProgress={scrollYProgress}
