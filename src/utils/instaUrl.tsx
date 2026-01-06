4bffc21ff0fddc91b8fd2dc825600fc0048a130d
Nicolasbuisson <nicolas.buisson@mail.mcgill.ca>
2026-01-06 17:06:48 -0500

    fix isIgMediaUrlValid helper function build error

--- src/utils/instaUrl.tsx
+++ src/utils/instaUrl.tsx
@@ -1,8 +1,8 @@
 // Check if the current time is before or at the expiration time
 export const isIgMediaUrlValid = (mediaUrl: string, till: Date = new Date()): boolean => {
-  let url = new URL(mediaUrl);
+  const url = new URL(mediaUrl);
   // Get the 'oe' parameter and parse it as a hex integer
-  let urlExpiryTimestamp = parseInt(url.searchParams.get("oe") ?? "0", 16);
+  const urlExpiryTimestamp = parseInt(url.searchParams.get("oe") ?? "0", 16);
   // for some reason, oe parameter only contains the time, so must multiply by 1000 to get the date
   return till < new Date(urlExpiryTimestamp * 1000); 
 }
